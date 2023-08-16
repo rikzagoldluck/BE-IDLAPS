ade48d32af2d3603356d05c595c4e3ad1ea19dee
imanuddin74 <imanuddin_ren@yahoo.com>
2023-08-16 23:06:35 +0700

    FF

--- src/index.js
+++ src/index.js
@@ -82,14 +82,14 @@ const getBeacons = async () => {
 
 client.on("connect", () => {
   console.log(`${protocol}: Connected`);
-  client.subscribe("silabs/aoa/angle/ble-pd-4C5BB3112B87/ble-pd-#", {
+  client.subscribe("silabs/aoa/angle/ble-pd-4C5BB3112B88/ble-pd-#", {
     //client.subscribe("silabs/aoa/position/multilocator-test_room/ble-pd-#", {
     qos,
   });
   getBeacons().then((res) => {
     res.forEach((beacon) => {
       client.subscribe(
-        `silabs/aoa/angle/ble-pd-4C5BB3112B87/ble-pd-${beacon.tag_id}`, //`silabs/aoa/position/multilocator-test_room/ble-pd-${beacon.tag_id}`,
+        `silabs/aoa/angle/ble-pd-4C5BB3112B88/ble-pd-${beacon.tag_id}`, //`silabs/aoa/position/multilocator-test_room/ble-pd-${beacon.tag_id}`,
         { qos },
         (error) => {
           if (error) {
@@ -97,7 +97,7 @@ client.on("connect", () => {
             return;
           } // untuk ANGLE
           console.log(
-            `${protocol}: Subscribe to topic 'silabs/aoa/angle/ble-pd-4C5BB3112B87/ble-pd-${beacon.tag_id}'` // Untuk POSITION xyz //console.log( //  `${protocol}: Subscribe to topic 'silabs/aoa/position/multilocator-test_room/ble-pd-${beacon.tag_id}'`
+            `${protocol}: Subscribe to topic 'silabs/aoa/angle/ble-pd-4C5BB3112B88/ble-pd-${beacon.tag_id}'` // Untuk POSITION xyz //console.log( //  `${protocol}: Subscribe to topic 'silabs/aoa/position/multilocator-test_room/ble-pd-${beacon.tag_id}'`
           );
         }
       );
@@ -130,7 +130,7 @@ client.on("message", (topic, payload) => {
       time = Date.now().toString(),
     } = JSON.parse(payload.toString()); //const { mac, y, x, time } = JSON.parse(payload.toString()); //console.log(Date.now().toString())
     console.log(mac, "  ", elevation, "  ", time); // RECORD ONLY IF Y AND TIME IS DIFFERENT // console.dir(JSON.parse(payload.toString()), {depth: "Infinity"})
-    if (parseFloat(elevation) < 75.0 || parseFloat(elevation) > 85.0) return; //if (parseFloat(x) < 0.0 || parseFloat(x) > 3.0) return;
+    if (parseFloat(elevation) < 70.0 || parseFloat(elevation) > 85.0) return; //if (parseFloat(x) < 0.0 || parseFloat(x) > 3.0) return;
     if (
       (lastData.mac !== mac ||
         lastData.elevation !== elevation ||
